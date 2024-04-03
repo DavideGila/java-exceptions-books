af8b54613065cd7fd4dfb039f70669cdc52d573b
DavideGila <davide.gila@gmail.com>
2024-04-03 15:44:01 +0200

    created the array books with a try-catch and try-catch for FileWriter which create the books.txt file and write in it the books written by the user

---
+++ src/org/learning/javaexceptionsbooks/Main.java
@@ -0,0 +1,61 @@
+package org.learning.javaexceptionsbooks;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quanti libri vuoi inserire? ");
+        int numBooks = Integer.parseInt(scanner.nextLine());
+
+        Book[] books = new Book[numBooks];
+
+        for (int i = 0; i < numBooks; i++) {
+            System.out.println("\nInserisci i dati del libro " + (i + 1) + ":");
+            try {
+                System.out.print("Titolo: ");
+                String title = scanner.nextLine();
+
+                System.out.print("Numero di pagine: ");
+                int numberOfPages = Integer.parseInt(scanner.nextLine());
+
+
+                System.out.print("Autore: ");
+                String author = scanner.nextLine();
+
+                System.out.print("Editore: ");
+                String publisher = scanner.nextLine();
+
+                books[i] = new Book(title, numberOfPages, author, publisher);
+            } catch (IllegalArgumentException e) {
+                // getMessage() è un un metodo definito nella un metodo definito nella classe Throwable che
+                // restituisce una stringa che descrive il messaggio associato all'eccezione.
+                System.out.println("Errore: " + e.getMessage());
+
+                System.out.println("Inserisci nuovamente i dati per il libro " + (i + 1));
+
+                // Torno indietro per reinserire i dati del libro corrente
+                i--;
+            }
+        }
+
+        scanner.close();
+
+        try (FileWriter writer = new FileWriter("books.txt")) {
+            for (Book book : books) {
+                if (book != null) {
+                    writer.write("Titolo: " + book.getTitle() + "- Numero di pagine: " + book.getNumberOfPages()
+                            + "- Autore: " + book.getAuthor() + "- Editore: " + book.getPublisher() + "\n");
+                }
+            }
+            System.out.println("Dati dei libri scritti nel file.");
+        } catch (IOException e) {
+            // IOException è una classe di eccezioni che viene utilizzata per gestire le eccezioni correlate all'I/O
+            // (Input/Output)
+            System.out.println("Si è verificato un errore durante la scrittura nel file: " + e.getMessage());
+        }
+    }
+}
